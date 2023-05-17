b5e56c5fc20b4d9eca225be9128d081eb52bb11d
wrinks <psellick76@gmail.com>
2023-05-17 13:52:40 -0400

    Updates after migration and stuffs

--- app.py
+++ app.py
@@ -4,9 +4,8 @@ from flask import Flask, jsonify
 from flask_smorest import Api
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
+from dotenv import load_dotenv
 from blocklist import BLOCKLIST
-
-
 import models
 
 
@@ -20,6 +19,7 @@ from resources.user import blp as UserBlueprint
 
 def create_app(db_url=None):
     app = Flask(__name__)
+    load_dotenv()
     app.config["API_TITLE"] = "Stores REST API"
     app.config["API_VERSION"] = "v1"
     app.config["OPENAPI_VERSION"] = "3.0.3"
@@ -54,10 +54,53 @@ def create_app(db_url=None):
         return (
             jsonify(
                 {"message": "The token has expired.", "error": "token_expired"}
-            ),
-            401,
+            )
         )
     
+import os
+from flask import Flask, jsonify
+from flask_smorest import Api
+from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
+from dotenv import load_dotenv
+from blocklist import BLOCKLIST
+import models
+
+
+
+from db import db
+from resources.item import blp as ItemBlueprint
+from resources.store import blp as StoreBlueprint
+from resources.tag import blp as TagBlueprint
+from resources.user import blp as UserBlueprint
+
+
+def create_app(db_url=None):
+    app = Flask(__name__)
+    load_dotenv()
+    app.config["API_TITLE"] = "Stores REST API"
+    app.config["API_VERSION"] = "v1"
+    app.config["OPENAPI_VERSION"] = "3.0.3"
+    app.config["OPENAPI_URL_PREFIX"] = "/"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    app.config[
+        "OPENAPI_SWAGGER_UI_URL"
+    ] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["PROPAGATE_EXCEPTIONS"] = True
+    db.init_app(app)
+    migrate = Migrate(app, db)
+    api = Api(app)
+
+    app.config["JWT_SECRET_KEY"] = "66663493481191115502769526685291581929"
+    jwt = JWTManager(app)
+
+    @jwt.token_in_blocklist_loader
+    def check_if_tokjen_in_blocklist(jwt_header, jwt_payload):
+        return jwt_payload["jti"] in BLOCKLIST
+
+
     @jwt.invalid_token_loader
     def invalid_token_callback(error):
         return(
--- migrations/env.py
+++ migrations/env.py
@@ -65,7 +65,10 @@ def run_migrations_offline():
     """
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
-        url=url, target_metadata=get_metadata(), literal_binds=True
+        url=url, 
+        target_metadata=get_metadata(), 
+        compare_type=True, 
+        literal_binds=True
     )
 
     with context.begin_transaction():
@@ -97,6 +100,7 @@ def run_migrations_online():
             connection=connection,
             target_metadata=get_metadata(),
             process_revision_directives=process_revision_directives,
+            compare_type=True,
             **current_app.extensions['migrate'].configure_args
         )
 
