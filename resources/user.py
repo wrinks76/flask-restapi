4f0370ec07a45b73cb9dfbef6c247af090e768f8
wrinks <psellick76@gmail.com>
2023-06-08 15:11:39 -0400

    Added mailgun code

--- resources/user.py
+++ resources/user.py
@@ -1,3 +1,5 @@
+import requests
+import os
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from passlib.hash import pbkdf2_sha256
@@ -16,6 +18,17 @@ from blocklist import BLOCKLIST
 
 
 blp = Blueprint("Users", "users", description="Operations on users")
+
+
+def send_simple_message(to, subject, body):
+    domain = os.getenv("MAILGUN_DOMAIN")
+    return requests.post(
+		f"https://api.mailgun.net/v3/{domain}messages",
+		auth=("api", os.getenv("MAILGUN_API_KEY")),
+		data={"from": "Excited User <mailgun@{domain}}>",
+			"to": [to],
+			"subject": subject,
+			"text": body})
 
 
 @blp.route("/login")
